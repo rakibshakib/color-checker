f42b1e88d2db887159964826f5ec10c51c5642d5
rakibshakib <rakibshakib74@gmail.com>
2021-06-30 01:39:04 +0600

    modified app.js file

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const backgroundColorEvent = document.getElementById('containerBg');
 colorInputEvent.addEventListener('input', event => {
     const color = event.target.value
     const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
-    if(color.match(hexColorRegex)){
-        // colorInputEvent.style.borderColor = color
+    if (color.match(hexColorRegex)) {
+
         backgroundColorEvent.style.backgroundColor = color
-    } else{
-        // colorInputEvent.style.borderColor = 'rgb(226,226,226)'
+    } else {
+
         backgroundColorEvent.style.backgroundColor = 'rgb(226,226,226)'
     }
 })
